7b5130edea3d0f3683eafc5dd131c4113b3cb80b
johangarciaccu-sys <johan.garciaccu@cun.edu.co>
2025-08-12 16:18:48 -0500

    Add files via upload

---
+++ Ejerciciosadicionales.java
@@ -0,0 +1,28 @@
+
+package ejerciciosadicionales;
+
+public class Ejerciciosadicionales {
+
+ 
+    public static void main(String[] args) {
+        System.out.println("Cesar, bienvenido al curso de programacion en java!!!");
+        System.out.println("""
+                            @@@@@@@
+                           @| O O |@
+                           (|  ^  |)
+                            | [_] |
+                            +-----+""");
+        System.out.println("""
+                            *
+                           ***
+                          *****
+                         *******
+                          *****
+                           ***
+                            *""");
+        System.out.println(" Adios"
+                + " y"
+                + " Hola");
+    }
+    
+}
